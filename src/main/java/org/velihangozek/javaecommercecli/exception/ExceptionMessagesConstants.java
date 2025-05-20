fbe51fec019c798dc3448b810934fbbdac0fe028
Velihan Gözek <velihangozek@hotmail.com>
2025-05-20 13:34:42 +0300

    add exception mechanism and messages

---
+++ src/main/java/org/velihangozek/javaecommercecli/exception/ExceptionMessagesConstants.java
@@ -0,0 +1,12 @@
+package org.velihangozek.javaecommercecli.exception;
+
+public class ExceptionMessagesConstants {
+
+
+    public static final String CUSTOMER_EMAIL_ALREADY_EXISTS = "Customer email already exists!";
+    public static final String CUSTOMER_EMAIL_DOES_NOT_EXIST = "Customer email does not exist!";
+    public static final String CUSTOMER_PASSWORD_DOES_NOT_MATCH = "Customer email or password does not match!";
+
+    private ExceptionMessagesConstants() {
+    }
+}
---
+++ src/main/java/org/velihangozek/javaecommercecli/exception/VeloStoreException.java
@@ -0,0 +1,7 @@
+package org.velihangozek.javaecommercecli.exception;
+
+public class VeloStoreException extends RuntimeException{
+    public VeloStoreException(String message) {
+        super(message);
+    }
+}
